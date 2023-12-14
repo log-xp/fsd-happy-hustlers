8da7ca2dbf900f660ade6ee1e8307d0e8abf45f5
karthik raja <bkarthikraja03@gmail.com>
2023-12-15 00:21:07 +0530

    2:12:03

--- api/index.js
+++ api/index.js
@@ -102,5 +102,11 @@ wss.on('connection',(connection,req) => {
       }
     }
   }
-  console.log([...wss.clients].map(c => c.username));
+  
+  [...wss.clients].forEach(client => {
+    client.send(JSON.stringify({
+      online: [...wss.clients].map(c => ({userId:c.userId,username:c.username})),
+    }
+    ));
+  });
 });
