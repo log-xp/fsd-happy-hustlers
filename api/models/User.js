786673dc4a35214abd1f0a3924a00ba90f3a69c0
karthik raja <bkarthikraja03@gmail.com>
2023-12-21 11:13:44 +0530

    database updated to store usertype and value

--- api/models/User.js
+++ api/models/User.js
@@ -1,10 +1,15 @@
 const mongoose =  require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-    username: {type:String , unique:true},
-    password : String,
-},{timestamp:true}) ;
+    username: { type: String, unique: true },
+    password: String,
+    fullName: String,
+    userType: String,
+    score: { type: Number, required: function() { return this.userType === 'Student'; } },
+    favSubject: { type: String, required: function() { return this.userType === 'Student'; } },
+    collegeGPA: { type: Number, required: function() { return this.userType === 'Guide'; } },
+    expertise: { type: String, required: function() { return this.userType === 'Guide'; } }
+}, { timestamps: true });
 
-
-const UserModel = mongoose.model('User',UserSchema);
+const UserModel = mongoose.model('User', UserSchema);
 module.exports = UserModel;
